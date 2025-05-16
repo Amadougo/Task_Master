7dabde959eabcd41082743560e500fc7fd9a1880
ALBERTS Greg <greg77290@yahoo.fr>
2025-05-16 14:41:53 +0200

    Modifications sur main.py + GUI.py => (Ajout du code de la classe Pression).

--- GUI.py
+++ GUI.py
@@ -1,12 +1,12 @@
 from tkinter import * # type: ignore
-from treatment import recuperer_donnees_onduleur
-
-compteur = 0
+from treatment import recuperer_donnees_onduleur, recuperer_donnees_pression
+#from PIL import Image, ImageTk
 
 class Gui:
-    def __init__(self, onduleur):
+    def __init__(self, onduleur, pression):
         self.window = Tk()  # Creation of the window (Graphical User Interface)
         self.onduleur = onduleur  # Creation of the onduleur object
+        self.pression = pression # Creation of the pression object
         self.setup_gui()  # Initial configuration of the GUI
 
     def setup_gui(self):
@@ -40,10 +40,39 @@ class Gui:
         self.box1_1.grid(row=0, column=0, sticky='nsew')
         self.box1_2.grid(row=0, column=1, sticky='nsew')
 
-        # Add labels inside the boxes
-        self.text_box1_1 = Label(self.box1_1, text="text_box1_1", bg='#64698A', fg='white', font=('Helvetica', 16, 'bold italic'))
-        self.text_box1_2 = Label(self.box1_2, text="text_box1_2", bg='#64698A', fg='black', font=('Helvetica', 16, 'bold italic'))
-        self.text_box1_1.pack(expand=YES)
+        # Add labels inside box1_1
+        # Onduleur data
+        self.text1_box1_1 = Label(self.box1_1, text="text1_box1_1", bg='#64698A', fg='white', font=('Helvetica', 12, 'bold italic'))
+        self.text2_box1_1 = Label(self.box1_1, text="text2_box1_1", bg='#64698A', fg='white', font=('Helvetica', 12, 'bold italic'))
+        self.text3_box1_1 = Label(self.box1_1, text="text3_box1_1", bg='#64698A', fg='white', font=('Helvetica', 12, 'bold italic'))
+        self.text4_box1_1 = Label(self.box1_1, text="text4_box1_1", bg='#64698A', fg='white', font=('Helvetica', 12, 'bold italic'))
+        self.text5_box1_1 = Label(self.box1_1, text="text5_box1_1", bg='#64698A', fg='white', font=('Helvetica', 12, 'bold italic'))
+        self.text6_box1_1 = Label(self.box1_1, text="text6_box1_1", bg='#64698A', fg='white', font=('Helvetica', 12, 'bold italic'))
+        self.text7_box1_1 = Label(self.box1_1, text="text7_box1_1", bg='#64698A', fg='white', font=('Helvetica', 12, 'bold italic'))
+
+        # Pression data
+        self.text8_box1_1 = Label(self.box1_1, text="text8_box1_1", bg='#64698A', fg='white', font=('Helvetica', 12, 'bold italic'))
+        self.text9_box1_1 = Label(self.box1_1, text="text9_box1_1", bg='#64698A', fg='white', font=('Helvetica', 12, 'bold italic'))
+        self.text10_box1_1 = Label(self.box1_1, text="text10_box1_1", bg='#64698A', fg='white', font=('Helvetica', 12, 'bold italic'))
+        self.text11_box1_1 = Label(self.box1_1, text="text11_box1_1", bg='#64698A', fg='white', font=('Helvetica', 12, 'bold italic'))
+        self.text12_box1_1 = Label(self.box1_1, text="text12_box1_1", bg='#64698A', fg='white', font=('Helvetica', 12, 'bold italic'))
+
+        self.text1_box1_1.pack(expand=YES)
+        self.text2_box1_1.pack(expand=YES)
+        self.text3_box1_1.pack(expand=YES)
+        self.text4_box1_1.pack(expand=YES)
+        self.text5_box1_1.pack(expand=YES)
+        self.text6_box1_1.pack(expand=YES)
+        self.text7_box1_1.pack(expand=YES)
+        self.text8_box1_1.pack(expand=YES)
+        self.text9_box1_1.pack(expand=YES)
+        self.text10_box1_1.pack(expand=YES)
+        self.text11_box1_1.pack(expand=YES)
+        self.text12_box1_1.pack(expand=YES)
+
+        # Add labels inside box1_2
+        self.text_box1_2 = Label(self.box1_2, text="text1_box1_2", bg='#64698A', fg='black', font=('Helvetica', 16, 'bold italic'))
+        
         self.text_box1_2.pack(expand=YES)
 
         # Configuration of a grid inside box2
@@ -67,11 +96,11 @@ class Gui:
         self.box2_5.grid(row=0, column=4, sticky='nsew')
 
         # Add button inside each box
-        self.button_box2_1 = Button(self.box2_1, text="button_box2_1", bg='#64698A', fg='lightgreen', font=('Helvetica', 16))
+        self.button_box2_1 = Button(self.box2_1, text="button_box2_1", bg='#64698A', fg='red', font=('Helvetica', 16))
         self.button_box2_2 = Button(self.box2_2, text="button_box2_2", bg='#64698A', fg='orange', font=('Helvetica', 16))
         self.button_box2_3 = Button(self.box2_3, text="button_box2_3", bg='#64698A', fg='orange', font=('Helvetica', 16))
         self.button_box2_4 = Button(self.box2_4, text="button_box2_4", bg='#64698A', fg='orange', font=('Helvetica', 16))
-        self.button_box2_5 = Button(self.box2_5, text="button_box2_5", bg='#64698A', fg='red', font=('Helvetica', 16))
+        self.button_box2_5 = Button(self.box2_5, text="button_box2_5", bg='#64698A', fg='lightgreen', font=('Helvetica', 16))
         self.button_box2_1.pack(expand=YES)
         self.button_box2_2.pack(expand=YES)
         self.button_box2_3.pack(expand=YES)
@@ -83,22 +112,23 @@ class Gui:
 
         
     def update_gui(self):
-        global compteur
-        
-        print("Passer par là !")
-        
         # Get the data from onduleur
         recuperer_donnees_onduleur(self.onduleur)
+        recuperer_donnees_pression(self.pression)
 
-        # Update the widgets of window
-        self.text_box1_1.config(text=f"Tension d'entrée : {self.onduleur.input_voltage} V")
-        
-        if(compteur > 4):
-            self.text_box1_2.config(text="Mise à jour_box1_2")
-        else:
-            compteur += 1
-
-        print("Repasser par là !")
+        # Update the widgets of box1_1
+        self.text1_box1_1.config(text=f"Tension d'entrée (input_voltage) : {self.onduleur.input_voltage} V")
+        self.text2_box1_1.config(text=f"Fréquence d'entrée (input_frequency) : {self.onduleur.input_frequency} Hz")
+        self.text3_box1_1.config(text=f"Tension de la batterie (battery_voltage) : {self.onduleur.battery_voltage} V")
+        self.text4_box1_1.config(text=f"Temps avant extinction de la batterie (battery_runtime) : {self.onduleur.battery_runtime} s")
+        self.text5_box1_1.config(text=f"Charge de la batterie (battery_charge) : {self.onduleur.battery_charge} %")
+        self.text6_box1_1.config(text=f"Charge ups (ups_load) : {self.onduleur.ups_load} %")
+        self.text7_box1_1.config(text=f"Statut ups (ups_status) : {self.onduleur.ups_status}")
+        self.text8_box1_1.config(text=f"Pression de la 1ère pompe Turbo (Jauge_1_Turbo) : {self.pression.Jauge_1_Turbo} Torr")
+        self.text9_box1_1.config(text=f"Pression de la 2nde pompe Turbo (Jauge_2_Turbo) : {self.pression.Jauge_2_Turbo} Torr")
+        self.text10_box1_1.config(text=f"Pression de la 3ème pompe Turbo (Jauge_3_Turbo) : {self.pression.Jauge_3_Turbo} Torr")
+        self.text11_box1_1.config(text=f"Pression de la 4ème pompe Turbo (Jauge_4_Turbo) : {self.pression.Jauge_4_Turbo} Torr")
+        self.text12_box1_1.config(text=f"Pression de la pompe primaire (Jauge_5_Primaire) : {self.pression.Jauge_5_Primaire} Torr")
         
         # Callback of this update function after 1 seconde
         self.window.after(1000, self.update_gui)
@@ -106,97 +136,3 @@ class Gui:
     def run(self):
         # Display of the window
         self.window.mainloop()
-
-"""
-text_box1_1 = None 
-text_box1_2 = None
-
-# Creation of the window (Graphical User Interface)
-window = Tk()
-
-def setup_gui(onduleur):
-    global text_box1_1, text_box1_2
-    # Get the size of the screen
-    screen_width = window.winfo_screenwidth()
-    screen_height = window.winfo_screenheight()
-
-    # Edit of the main features for the window
-    window.attributes('-fullscreen', YES)
-    window.configure(bg='#64698A')
-
-    # Configuration of the main grid (to place boxes in)
-    window.grid_rowconfigure(index=0, weight=9) # 1st row takes 90% of the screen
-    window.grid_rowconfigure(index=1, weight=1) # 2nd row takes 10% of the screen
-    window.grid_columnconfigure(index=0, weight=1) # 1st and only column takes the whole space
-
-    # Creation of 2 boxes
-    box1 = Frame(window, bg='#64698A', bd=0)
-    box2 = Frame(window, bg='#64698A', bd=0)
-    box1.grid(row=0, column=0, sticky='nsew')
-    box2.grid(row=1, column=0, sticky='nsew')
-
-    # Configuration of a grid inside box1
-    box1.grid_rowconfigure(index=0, weight=1)
-    box1.grid_columnconfigure(index=0, weight=1)
-    box1.grid_columnconfigure(index=1, weight=1)
-
-    # Creation of 2 boxes inside box1
-    box1_1 = Frame(box1, bg='#64698A', bd=0)
-    box1_2 = Frame(box1, bg='#64698A', bd=0)
-    box1_1.grid(row=0, column=0, sticky='nsew')
-    box1_2.grid(row=0, column=1, sticky='nsew')
-
-    # Add labels inside the boxes
-    text_box1_1 = Label(box1_1, text="text_box1_1", bg='#64698A', fg='white', font=('Helvetica', 16, 'bold italic'))
-    text_box1_2 = Label(box1_2, text="text_box1_2", bg='#64698A', fg='black', font=('Helvetica', 16, 'bold italic'))
-    text_box1_1.pack(expand=YES)
-    text_box1_2.pack(expand=YES)
-
-    # Configuration of a grid inside box2
-    box2.grid_rowconfigure(index=0, weight=1)
-    box2.grid_columnconfigure(index=0, weight=1)
-    box2.grid_columnconfigure(index=1, weight=1)
-    box2.grid_columnconfigure(index=2, weight=1)
-    box2.grid_columnconfigure(index=3, weight=1)
-    box2.grid_columnconfigure(index=4, weight=1)
-
-    # Creation of 5 boxes inside box2
-    box2_1 = Frame(box2, bg='#64698A', bd=0)
-    box2_2 = Frame(box2, bg='#64698A', bd=0)
-    box2_3 = Frame(box2, bg='#64698A', bd=0)
-    box2_4 = Frame(box2, bg='#64698A', bd=0)
-    box2_5 = Frame(box2, bg='#64698A', bd=0)
-    box2_1.grid(row=0, column=0, sticky='nsew')
-    box2_2.grid(row=0, column=1, sticky='nsew')
-    box2_3.grid(row=0, column=2, sticky='nsew')
-    box2_4.grid(row=0, column=3, sticky='nsew')
-    box2_5.grid(row=0, column=4, sticky='nsew')
-
-    # Add button inside each box
-    button_box2_1 = Button(box2_1, text="button_box2_1", bg='#64698A', fg='lightgreen', font=('Helvetica', 16))
-    button_box2_2 = Button(box2_2, text="button_box2_2", bg='#64698A', fg='orange', font=('Helvetica', 16))
-    button_box2_3 = Button(box2_3, text="button_box2_3", bg='#64698A', fg='orange', font=('Helvetica', 16))
-    button_box2_4 = Button(box2_4, text="button_box2_4", bg='#64698A', fg='orange', font=('Helvetica', 16))
-    button_box2_5 = Button(box2_5, text="button_box2_5", bg='#64698A', fg='red', font=('Helvetica', 16))
-    button_box2_1.pack(expand=YES)
-    button_box2_2.pack(expand=YES)
-    button_box2_3.pack(expand=YES)
-    button_box2_4.pack(expand=YES)
-    button_box2_5.pack(expand=YES)
-    
-    # Initialisation of the GUI
-    update_gui(onduleur)
-
-    # Display of the window
-    window.mainloop()
-
-# -------- Management of the GUI's update -------- #
-def update_gui(onduleur):
-    global text_box1_1, text_box1_2
-    print("Passer par là !")
-    recuperer_donnees_onduleur(onduleur)
-    text_box1_1.config(text=f"Tension d'entrée : {onduleur.input_voltage} V")
-    text_box1_2.config(text="Mise à jour_box1_2")
-    print("Repasser par là !")
-    window.after(1000, update_gui, onduleur)
-"""
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from data import Onduleur
-from treatment import recuperer_donnees_onduleur
+from data import Onduleur, Pression
+from treatment import recuperer_donnees_onduleur, recuperer_donnees_pression
 from GUI import Gui # import the GUI class from GUI.py
 
+# Onduleur
 onduleur = Onduleur()
 recuperer_donnees_onduleur(onduleur)
 
+# Pression
+pression = Pression()
+recuperer_donnees_pression(pression)
+
 # Creation of the GUI
-gui = Gui(onduleur)
+gui = Gui(onduleur, pression)
 
 # Run the user interface
 gui.run()
