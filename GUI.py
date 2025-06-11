a68ff9b68a31bf19f6009af4a7a7a13441cff89a
ALBERTS Greg <greg77290@yahoo.fr>
2025-06-11 10:59:22 +0200

    mode fenetré.

--- GUI.py
+++ GUI.py
@@ -17,7 +17,7 @@ class Gui:
         screen_height = self.window.winfo_screenheight()
 
         # Edit of the main features for the window
-        self.window.attributes('-fullscreen', YES)
+        self.window.attributes('-fullscreen', NO)
         self.window.configure(bg='#64698A')
 
         # Configuration of the main grid (to place boxes in)
