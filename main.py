255574641d1e38d90a635056e3252050b80f2d02
ALBERTS Greg <greg77290@yahoo.fr>
2025-06-20 10:13:22 +0200

    Ajout pop up centrée.

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-# from data import Onduleur, Pression
-# #from treatment import recuperer_donnees_onduleur, recuperer_donnees_pression_jauge1, recuperer_donnees_pression_jauge2, recuperer_donnees_pression_jauge3, recuperer_donnees_pression_jauge4, recuperer_donnees_pression_jauge5, recuperer_donnees_pression_jauge6
-# from GUI import Gui # import the GUI class from GUI.py
+from data import Onduleur, Pression
+#from treatment import recuperer_donnees_onduleur, recuperer_donnees_pression_jauge1, recuperer_donnees_pression_jauge2, recuperer_donnees_pression_jauge3, recuperer_donnees_pression_jauge4, recuperer_donnees_pression_jauge5, recuperer_donnees_pression_jauge6
+from GUI import Gui # import the GUI class from GUI.py
 
-# # Creation of an Onduleur object
-# onduleur = Onduleur()
+# Creation of an Onduleur object
+onduleur = Onduleur()
 
-# # Creation of a Pression object
-# pression = Pression()
+# Creation of a Pression object
+pression = Pression()
 
-# # Creation of the GUI
-# gui = Gui(onduleur, pression, affichage_donnees=True)
+# Creation of the GUI
+gui = Gui(onduleur, pression, affichage_donnees=True)
 
-# # Get Onduleur and Pression data
-# #gui.recuperer_donnees(onduleur, pression)
+# Get Onduleur and Pression data
+#gui.recuperer_donnees(onduleur, pression)
 
-# """# Rescale images
-# #gui.resize_images()"""
+"""# Rescale images
+#gui.resize_images()"""
 
-# # Force the resize of boxes dimensions
-# gui.force_initial_resizing()
+# Force the resize of boxes dimensions
+gui.force_initial_resizing()
 
-# # Run the user interface
-# gui.run()
+# Run the user interface
+gui.run()
