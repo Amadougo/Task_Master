499d3e378ac55d602764683452675c689166be08
ALBERTS Greg <greg77290@yahoo.fr>
2025-07-01 10:41:36 +0200

    remise plein écran.

--- GUI.py
+++ GUI.py
@@ -29,10 +29,11 @@ class Gui:
         self.window.title("Interface graphique du programme de sécurité OIA")
 
         # Edit of the main features for the window
-        self.window.geometry(f"{self.screen_width}x{self.screen_height}")
+        self.window.config('-fullscreen', YES)
+        #self.window.geometry(f"{self.screen_width}x{self.screen_height}")
         self.window.configure(bg='#64698A')
         # Redéfinir le comportement de fermeture
-        self.window.protocol("WM_DELETE_WINDOW", self.popUpConfirmationQuitterApplication)
+        #self.window.protocol("WM_DELETE_WINDOW", self.popUpConfirmationQuitterApplication)
 
         # Configuration of the main grid (to place boxes in)
         self.window.grid_rowconfigure(index=0, weight=9) # 1st row takes 90% of the screen
