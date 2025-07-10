d4955712e23bb1b5b7eb6189a1baa89727e6fd69
Amadougo <hugo.l@me.com>
2025-07-10 15:01:13 +0200

    Refactor la fonction de sécurité pour utiliser des méthodes d'instance et simplifier la gestion des relais

--- securite.py
+++ securite.py
@@ -1,34 +1,39 @@
 from data import EtatManip, Pression, Onduleur
 from math import pow
-from treatment import relais1_OFF, relais1_ON, relais2_OFF, relais2_ON
+from treatment import relais1et2_OFF, relais1et2_ON
 from treatment import pompe_SCU_1400_1_ON, pompe_SCU_1400_1_OFF, pompe_SCU_1400_2_ON, pompe_SCU_1400_2_OFF, pompe_SCU_800_OFF, pompe_SCU_800_ON
 import subprocess
 import time
 
 PRESSION_SEUIL_PRIMAIRE = pow(10,-2) #milibar
+class Securite:
+    def __init__(self, etat_manip, pression, onduleur1, onduleur2):
+        self.etat_manip = etat_manip
+        self.pression = pression
+        self.onduleur1 = onduleur1
+        self.onduleur2 = onduleur2
 
-def securite(etat_manip: EtatManip, pression: Pression, onduleur1 : Onduleur, onduleur2 : Onduleur) :
+def securite(self) :
     
     print(f"{time.monotonic()}, passage dans la securite")
-    print(f"Action confirmée. {etat_manip}")
+    print(f"Action confirmée. {self.etat_manip}")
     
     #Actions lorsque la manip est en 'OFF'
-    if (etat_manip == EtatManip.OFF) :
+    if (self.etat_manip == EtatManip.OFF) :
         print("État manip : 0FF")
         #print(f"seuil primaire défini à {PRESSION_SEUIL_PRIMAIRE}")
         #Exctinction de l'ordinateur en cas de coupure de courant prolongée
-        if (int(onduleur2.battery_runtime) < 300) :  # 300 secondes = 5 minutes
+        if (int(self.onduleur2.battery_runtime) < 300) :  # 300 secondes = 5 minutes
             print("Coupure de courant prolongée, extinction de l'ordinateur")
             #Besoin des droits sudoers, à voir si on peut démarrer le programme avec les droits sudoers
             # ou bien si on peut désactiver les droits sudoers pour shutdown
             subprocess.run(["sudo", "shutdown", "-h", "now"])
 
     #Actions lorsque la manip est en 'Démarrage'
-    elif (etat_manip == EtatManip.DEMARRAGE) :
+    elif (self.etat_manip == EtatManip.DEMARRAGE) :
         print("État manip : Démarrage…")
         #On démarre les pompes finales (relais)
-        relais1_ON()
-        relais2_ON()
+        relais1et2_ON()
         #On démarre les pompes secondaires
         pompe_SCU_1400_1_ON()
         pompe_SCU_1400_2_ON()
@@ -38,33 +43,32 @@ def securite(etat_manip: EtatManip, pression: Pression, onduleur1 : Onduleur, on
         '''vérifier pompes primaires'''
 
     #Actions lorsque la manip est en 'Fonctionnement'
-    elif (etat_manip == EtatManip.FONCTIONNE) :
+    elif (self.etat_manip == EtatManip.FONCTIONNE) :
         print("État manip : Fonctionne")
 
         #Première sécurité si la pression primaire est trop faible ou bien que la jauge est déconnectée
-        if (pression.Jauge_5_Primaire == "Déconnectée" or pression.Jauge_5_Primaire == "Validation manuelle requise") :
+        if (self.pression.Jauge_5_Primaire == "Déconnectée" or self.pression.Jauge_5_Primaire == "Validation manuelle requise") :
             print("Erreur jauge primaire, arrêt des pompes")
             etat_manip = EtatManip.ARRET_EN_COURS
         else :
-            value = pression.Jauge_5_Primaire.split(" ")      
+            value = self.pression.Jauge_5_Primaire.split(" ")      
             flt = float(value[0])
             if (flt > PRESSION_SEUIL_PRIMAIRE) :
                 etat_manip = EtatManip.ARRET_EN_COURS
         #Deuxième sécurité en cas de coupure de courant de plus de 10min
-        if (int(onduleur1.battery_runtime) < 240) :
+        if (int(self.onduleur1.battery_runtime) < 240) :
             etat_manip = EtatManip.ARRET_EN_COURS
         
 
     #Actions lorsque la manip est en 'cours d'arrêt'
-    elif (etat_manip == EtatManip.ARRET_EN_COURS) :
+    elif (self.etat_manip == EtatManip.ARRET_EN_COURS) :
         print("État manip : Arrêt en cours")
         #On arrête les pompes finales (relais)
-        relais1_OFF()
-        relais2_OFF()
+        relais1et2_OFF()
         #On arrête les pompes secondaires
         pompe_SCU_1400_1_OFF()
         pompe_SCU_1400_2_OFF()
         pompe_SCU_800_OFF()
         etat_manip = EtatManip.OFF
-        
+
     print(f"{time.monotonic()}, fin du passage dans la securite")
