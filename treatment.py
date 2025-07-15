2164cc664fc0735d2eb99f99ae6757ed4120dc3a
ALBERTS Greg <greg77290@yahoo.fr>
2025-07-15 14:28:42 +0200

    Ajout d'un affichage de la réponse dans la fonction controle_cathode pour améliorer le débogage

--- treatment.py
+++ treatment.py
@@ -256,7 +256,8 @@ def controle_cathode(cathode: Cathode):
     command = "I?\n"
     serial_cathode.write(command.encode())
     response = serial_cathode.readline().decode().strip()
-    if(not response or response[0] != "I"):
+    print(f"reponse = {response}")
+    if(response == '' or response[0] != "I"):
        cathode.etat = EtatCathode.DECONNECTEE
     elif(cathode.etat == EtatCathode.DECONNECTEE):
         cathode.etat = EtatCathode.FROIDE # Seulement pour la première où l'appareil est reconnecté/rallumé
