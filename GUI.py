4657e41226b33d190fb2b5388a3ac368edf674b6
ALBERTS Greg <greg77290@yahoo.fr>
2025-07-22 11:13:49 +0200

    5

--- GUI.py
+++ GUI.py
@@ -527,8 +527,8 @@ class Gui:
             self.label_image_box1_1_3.image = self.tk_resized_image_box1_1_3
             
     def resize_image_box1_1_4(self, event=None):
-        width = self.box1_1_4.winfo_width()
-        height = self.box1_1_4.winfo_height()
+        width = 50
+        height = 40
         new_width = max(1, int(width * 0.3))
         new_height = max(1, int(height * 0.6))
         if width > 0 and height > 0:                
